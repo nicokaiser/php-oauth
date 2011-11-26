a03bc43744f8b400d99364c25b59daff743d2c8d
Vyacheslav Salakhutdinov <megazoll@gmail.com>
2011-11-26 15:15:50 +0400

    Changed signature comparing to be timeing insensitive

--- lib/OAuth/SignatureMethod/SignatureMethod.php
+++ lib/OAuth/SignatureMethod/SignatureMethod.php
@@ -47,6 +47,22 @@ abstract class SignatureMethod
     public function checkSignature($request, $consumer, $token, $signature)
     {
         $built = $this->buildSignature($request, $consumer, $token);
-        return $built == $signature;
+        
+        // Check for zero length, although unlikely here
+        if (strlen($built) == 0 || strlen($signature) == 0) {
+            return false;
+        }
+
+        if (strlen($built) != strlen($signature)) {
+            return false;
+        }
+
+        // Avoid a timing leak with a (hopefully) time insensitive compare
+        $result = 0;
+        for ($i = 0; $i < strlen($signature); $i++) {
+            $result |= ord($built{$i}) ^ ord($signature{$i});
+        }
+
+        return $result == 0;
     }
 }
